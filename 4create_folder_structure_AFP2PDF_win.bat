f9e22c847ab0fdd69537f49cdb6589e3ce25258c
CodrutaGit <64213593+CodrutaGit@users.noreply.github.com>
2020-10-07 14:49:13 +0300

    Add files via upload

---
+++ 4create_folder_structure_AFP2PDF_win.bat
@@ -0,0 +1,20 @@
+@ECHO OFF
+
+Set dir=E:\OutputTransformAFP2PDF\win\output\
+Echo Deleting all files from %dir%
+del %dir%\* /F /Q
+
+Echo Deleting all folders from %dir%
+for /d %%p in (%dir%\*) Do rd /Q /S "%%p"
+@echo Folder deleted.
+
+mkdir E:\OutputTransformAFP2PDF\win\output\output_PDF\TestFiles\
+echo mkdir E:\OutputTransformAFP2PDF\win\output\output_PDF\TestFiles\
+
+mkdir E:\OutputTransformAFP2PDF\win\output\output_TIFF\MF\
+echo mkdir E:\OutputTransformAFP2PDF\win\output\output_TIFF\MF\
+
+c:\Windows\System32\xcopy.exe /T /e e:\TestFiles_folder_structure\TestFiles\* E:\OutputTransformAFP2PDF\win\output\output_PDF\TestFiles\ /s /i
+c:\Windows\System32\xcopy.exe /T /e e:\TestFiles_folder_structure\TestFiles\* E:\OutputTransformAFP2PDF\win\output\output_TIFF\MF\ /s /i
+
+echo Folder structure succesfully done!
