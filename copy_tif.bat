996a5ac7765d125f5453fa645baa1ce7288e7022
CodrutaGit <64213593+CodrutaGit@users.noreply.github.com>
2020-10-07 14:45:02 +0300

    Delete copy_tif.bat

--- copy_tif.bat
+++
@@ -1,2 +0,0 @@
-echo Copy.... e:\OutputTransformPDF2AFP_APPE\win\output\output_PDF\TestFiles\PDF\*.tif e:\OutputTransformPDF2AFP_APPE\win\output\output_TIFF\MasterFiles\PDF\
-xcopy /y /s /d  e:\OutputTransformPDF2AFP_APPE\win\output\output_PDF\TestFiles\PDF\*.tif e:\OutputTransformPDF2AFP_APPE\win\output\output_TIFF\MasterFiles\PDF\
