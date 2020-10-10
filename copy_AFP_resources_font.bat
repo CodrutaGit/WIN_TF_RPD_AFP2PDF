bda9850458c595e5d01f48364b08b84026dbd245
CodrutaGit <64213593+CodrutaGit@users.noreply.github.com>
2020-10-10 11:41:57 +0300

    Add files via upload

--- copy_AFP_resources_font.bat
+++ copy_AFP_resources_font.bat
@@ -1,11 +1,12 @@
 @echo off
 :: variables
 echo Backing up file
-set source="D:\Resources_AFP\font\*"
+set source="D:\Resources_AFP\font\*.*"
 Echo Source dir is: D:\Resources_AFP\font\
-set destination="C:\Program Files\InfoPrint\InfoPrint Transform Features\itm\xforms\afp2pdf\font\"
+set destination="%ProgramFiles%\InfoPrint\InfoPrint Transform Features\itm\xforms\afp2pdf\font\"
 Echo Destination dir is: C:\Program Files\InfoPrint\InfoPrint Transform Features\itm\xforms\afp2pdf\font\
-set xcopy=c:\Windows\System32\xcopy.exe /S/E/V/Q/F/H/I/N/Y
+
+set xcopy="c:\Windows\System32\xcopy.exe" /S/E/V/Q/F/H/I/N/Y
 Echo Copying resources from %source% %destination%
 %xcopy% %source% %destination%
 echo files will be copy press enter to proceed
