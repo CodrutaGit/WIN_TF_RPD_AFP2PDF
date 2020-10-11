4f2b2900d0f33290ff5e854bfb83836e0b777c35
CodrutaGit <64213593+CodrutaGit@users.noreply.github.com>
2020-10-11 18:10:49 +0300

    Add files via upload

--- copy_AFP_resources_font.bat
+++ copy_AFP_resources_font.bat
@@ -1,12 +1,9 @@
 @echo off
 :: variables
-cd "C:\Program Files\Resources_AFP\font\"
-
-net use \\172.27.123.44\qa\1-TESTFILES /u:saguaroprint.ro\test transforms /p:yes
 
 echo Backing up file
-set source="\\172.27.123.44\qa\1-RESURSE\TrueTypeFonts\*.*"
-Echo Source dir is: C:\Program Files\Resources_AFP\font\
+set source="D:\Resources_AFP\font*.*"
+Echo Source dir is: D:\Resources_AFP\font
 
 set destination="C:\Program Files\InfoPrint\InfoPrint Transform Features\itm\xforms\afp2pdf\font\"
 Echo Destination dir is: C:\Program Files\InfoPrint\InfoPrint Transform Features\itm\xforms\afp2pdf\font\
