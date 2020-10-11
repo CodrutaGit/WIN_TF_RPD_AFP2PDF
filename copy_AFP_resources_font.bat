b15469fc7e69b07a1bf77e6a9f4db97e28ef1080
CodrutaGit <64213593+CodrutaGit@users.noreply.github.com>
2020-10-11 20:05:26 +0300

    Add files via upload

--- copy_AFP_resources_font.bat
+++ copy_AFP_resources_font.bat
@@ -5,8 +5,8 @@ echo Backing up file
 set source="D:\Resources_AFP\font\*.*"
 Echo Source dir is: D:\Resources_AFP\font\
 
-set destination="C:\Program Files\InfoPrint\InfoPrint Transform Features\itm\xforms\afp2pdf\"
-Echo Destination dir is: C:\Program Files\InfoPrint\InfoPrint Transform Features\itm\xforms\afp2pdf\
+set destination="C:\Program Files\InfoPrint\InfoPrint Transform Features\itm\xforms\afp2pdf\font\"
+Echo Destination dir is: C:\Program Files\InfoPrint\InfoPrint Transform Features\itm\xforms\afp2pdf\font\
 
 set xcopy=xcopy /S/E/V/Q/F/H/I/N/Y
 
--- copy_AFP_resources_reslib.bat
+++ copy_AFP_resources_reslib.bat
@@ -3,8 +3,8 @@
 echo Backing up file
 set source="D:\Resources_AFP\reslib\*"
 Echo Source dir is: D:\Resources_AFP\reslib\
-set destination="C:\Program Files\InfoPrint\InfoPrint Transform Features\itm\xforms\afp2pdf\"
-Echo Destination dir is: C:\Program Files\InfoPrint\InfoPrint Transform Features\itm\xforms\afp2pdf\
+set destination="C:\Program Files\InfoPrint\InfoPrint Transform Features\itm\xforms\afp2pdf\reslib\"
+Echo Destination dir is: C:\Program Files\InfoPrint\InfoPrint Transform Features\itm\xforms\afp2pdf\reslib\
 set xcopy=xcopy /S/E/V/Q/F/H/I/N/Y
 Echo Copying resources from %source% %destination%
 %xcopy% %source% %destination%
