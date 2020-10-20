452f416da822ada6d26577b5762e2e50538261ab
TransformsGit <64213593+TransformsGit@users.noreply.github.com>
2020-10-20 09:26:36 +0300

    Add files via upload

--- 4create_folder_structure_AFP2PDF_win.bat
+++ 4create_folder_structure_AFP2PDF_win.bat
@@ -15,6 +15,6 @@ mkdir E:\OutputTransformAFP2PDF\win\output\out_TIFF\MF
 echo mkdir E:\OutputTransformAFP2PDF\win\output\out_TIFF\MF
 
 c:\Windows\System32\xcopy.exe /T /e e:\TestFiles_folder_structure\TestFiles\* E:\OutputTransformAFP2PDF\win\output\out_PDF\TestFiles\ /s /i
-c:\Windows\System32\xcopy.exe /T /e e:\TestFiles_folder_structure\TestFiles\* E:\OutputTransformAFP2PDF\win\output\out_TIFF\MF\ /s /i
+c:\Windows\System32\xcopy.exe /T /e e:\TestFiles_folder_structure\TestFiles\* E:\OutputTransformAFP2PDF\win\output\out_TIFF\MasterFiles\ /s /i
 
 echo Folder structure succesfully done!
