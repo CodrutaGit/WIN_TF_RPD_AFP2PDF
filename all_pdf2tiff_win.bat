6e34abd494f4f148aeda22468d1c10f77dc0dfd9
TransformsGit <64213593+TransformsGit@users.noreply.github.com>
2020-10-20 09:06:17 +0300

    Add files via upload

--- all_pdf2tiff_win.bat
+++ all_pdf2tiff_win.bat
@@ -23,8 +23,8 @@ ECHO File extention is **************: fileextension=%%~xi
 
 echo ================================================
 
-echo Copy.... e:\OutputTransformAFP2PDF\win\output\out_PDF\TestFiles\*.tif e:\OutputTransformAFP2PDF\win\output\out_TIFF\TestFiles\  
-xcopy /y /s /d  e:\OutputTransformAFP2PDF\win\output\out_PDF\TestFiles\*.tif e:\OutputTransformAFP2PDF\win\output\out_TIFF\TestFiles\ >> C:\Users\Administrator\Transforms\TestAFP2PDF\Log\log_win\all_PDF2TIFF_itm-driver.log 2>&1
+echo Copy.... e:\OutputTransformAFP2PDF\win\output\out_PDF\TestFiles\*.tif e:\OutputTransformAFP2PDF\win\output\out_TIFF\MS\  
+xcopy /y /s /d  e:\OutputTransformAFP2PDF\win\output\out_PDF\TestFiles\*.tif e:\OutputTransformAFP2PDF\win\output\out_TIFF\MS\ >> C:\Users\Administrator\Transforms\TestAFP2PDF\Log\log_win\all_PDF2TIFF_itm-driver.log 2>&1
 
 
 echo EXIT
